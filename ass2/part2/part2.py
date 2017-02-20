e39f0ed5bfcf85da9a3227fc2700f61ed6dcce38
Benoit Gaujac <benoitgaujac@eduroam-int-dhcp-97-65-59.ucl.ac.uk>
2017-02-20 14:56:39 +0000

    fixed learning rate

--- ass2/part2/part2.py
+++ ass2/part2/part2.py
@@ -14,7 +14,7 @@ import logging
 logging.basicConfig(filename='out.log', level=logging.DEBUG)
 
 import build_model
-#import inpainting
+import inpainting
 
 
 SOURCE_URL = 'http://yann.lecun.com/exdb/mnist/'
@@ -24,13 +24,13 @@ NUM_CHANNELS = 1
 PIXEL_DEPTH = 255
 VALIDATION_SIZE = 5000  # Size of the validation set.
 SEED = 66478  # Set to None for random seed.
-BATCH_SIZE = 512
-BATCH_SIZE_EVAL = 512
+BATCH_SIZE = 256
+BATCH_SIZE_EVAL = 2048
 nsample = 100
 nsamples = 11
 
-num_epochs = 60
-epochs_per_checkpoint = 2
+num_epochs = 51
+epochs_per_checkpoint =50
 
 from_pretrained_weights = True
 
@@ -46,10 +46,10 @@ parser.add_option('-s', '--mode', action='store', dest='mode',
 #lstm1l64u = {"name": "lstm1l64u", "cell": "LSTM", "layers": 1, "units":64, "init_learning_rate": 0.0002}
 #lstm1l128u = {"name": "lstm1l128u", "cell": "LSTM", "layers": 1, "units":128, "init_learning_rate": 0.0002}
 #lstm3l32u = {"name": "lstm3l32u", "cell": "LSTM", "layers": 3, "units":32, "init_learning_rate": 0.00095}
-gru1l32u = {"name": "gru1l32u", "cell": "GRU", "layers": 1, "units":32, "init_learning_rate": 0.005}
-gru1l64u = {"name": "gru1l64u", "cell": "GRU", "layers": 1, "units":64, "init_learning_rate": 0.0005}
-gru1l128u = {"name": "gru1l128u", "cell": "GRU", "layers": 1, "units":128, "init_learning_rate": 0.0005}
-gru3l32u = {"name": "gru3l32u", "cell": "GRU", "layers": 3, "units":32, "init_learning_rate": 0.005}
+gru1l32u = {"name": "gru1l32u", "cell": "GRU", "layers": 1, "units":32, "init_learning_rate": 0.2}
+gru1l64u = {"name": "gru1l64u", "cell": "GRU", "layers": 1, "units":64, "init_learning_rate": 0.001}
+gru1l128u = {"name": "gru1l128u", "cell": "GRU", "layers": 1, "units":128, "init_learning_rate": 0.001}
+gru3l32u = {"name": "gru3l32u", "cell": "GRU", "layers": 3, "units":32, "init_learning_rate": 0.01}
 """
 models = {"lstm1l32u": lstm1l32u,"lstm1l64u":lstm1l64u, "lstm1l128u": lstm1l128u,
         "lstm3l32u":lstm3l32u, "gru1l32u":gru1l32u, "gru1l64u":gru1l64u,
@@ -158,7 +158,6 @@ def get_batches(images, batch_size=BATCH_SIZE):
         batches.append(X_batch)
     return batches
 
-
 ######################################## Main ########################################
 def main(model_archi,train_data, validation_data, test_data, mode_):
     nn_model = model_archi["name"]
@@ -195,6 +194,7 @@ def main(model_archi,train_data, validation_data, test_data, mode_):
                                                 targets=eval_data_node[:,1:],
                                                 logits=eval_logits))
 
+    """
     ###### Create varaible for batch ######
     batch = tf.Variable(0, dtype=data_type())
     ###### CLearning rate decay ######
@@ -206,12 +206,18 @@ def main(model_archi,train_data, validation_data, test_data, mode_):
                     staircase=True)
     ###### Optimizer ######
     optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss,global_step=batch)
+    """
+    ###### Optimizer ######
+    learning_rate = tf.placeholder(tf.float32, shape=[])
+    optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss)
     ###### Predictions for the current training minibatch ######
     train_prediction = tf.sigmoid(logits)
     ###### Predictions for the validation ######
     eval_prediction = tf.sigmoid(eval_logits)
     ###### Saver ######
-    saver = tf.train.Saver(write_version=tf.train.SaverDef.V1)
+    #saver = tf.train.Saver(write_version=tf.train.SaverDef.V1)
+    saver = tf.train.Saver()
+
 
     ###### Create a local session to run the training ######
     with tf.Session() as sess:
@@ -226,37 +232,46 @@ def main(model_archi,train_data, validation_data, test_data, mode_):
                 tf.global_variables_initializer().run()
             else:
                 saver.restore(sess, DST)
-                tf.variables_initializer([batch,]).run()
+                tf.variables_initializer([learning_rate,]).run()
+                """
                 learning_rate = tf.train.exponential_decay(
                                 model_archi["init_learning_rate"],  # Base learning rate.
                                 batch * BATCH_SIZE,                 # Current index into the dataset.
                                 10*train_size,                       # Decay step.
                                 0.97,                               # Decay rate.
                                 staircase=True)
+                """
 
             # initialize performance indicators
+            loss_history = [10000.0,]
             best_train_loss, best_eval_loss = 10000.0, 10000.0
             best_train_acc, best_eval_acc = 0.0, 0.0
+            # init learning rate
+            lr = model_archi["init_learning_rate"]
             #training loop
-            print("Start training {}...".format(nn_model))
+            print("\nStart training {}...".format(nn_model))
             logging.info("Start training {}...".format(nn_model))
             for epoch in range(num_epochs):
                 start_time = time.time()
                 train_loss, train_acc = 0.0, 0.0
                 print("")
                 print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+                logging.info(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
                 Batches = get_batches(train_data, BATCH_SIZE)
                 for batch_ in Batches:
-                    feed_dict = {train_data_node: batch_}
+                    feed_dict = {train_data_node: batch_, learning_rate: lr}
                     # Run the optimizer to update weights.
                     sess.run(optimizer, feed_dict=feed_dict)
-                    l, lr, predictions = sess.run([loss, learning_rate, train_prediction], feed_dict=feed_dict)
+                    #l, lr, predictions = sess.run([loss, learning_rate, train_prediction], feed_dict=feed_dict)
+                    l, predictions = sess.run([loss, train_prediction], feed_dict=feed_dict)
                     # Update average loss and accuracy
                     train_loss += l / len(Batches)
                     train_acc += accuracy_logistic(predictions,batch_[:,1:]) / len(Batches)
                 # Print info for previous epoch
-                print("Epoch {} done, took {:.2f}s, learning rate: {:.2f}e-4".format(epoch,time.time()-start_time,lr*10000))
-                logging.info("Epoch {} done, took {:.2f}s, learning rate: {:.2f}e-4".format(epoch,time.time()-start_time,lr*10000))
+                #print("Epoch {} done, took {:.2f}s, learning rate: {:.2f}e-4".format(epoch,time.time()-start_time,lr*10000))
+                #logging.info("Epoch {} done, took {:.2f}s, learning rate: {:.2f}e-4".format(epoch,time.time()-start_time,lr*10000))
+                print("Epoch {} done, took {:.2f}s, learning rate: {:.2f}e-3".format(epoch,time.time()-start_time,lr*1000))
+                logging.info("Epoch {} done, took {:.2f}s, learning rate: {:.2f}e-3".format(epoch,time.time()-start_time,lr*1000))
                 if train_acc>best_train_acc:
                     best_train_acc = train_acc
                 if train_loss<best_train_loss:
@@ -265,6 +280,13 @@ def main(model_archi,train_data, validation_data, test_data, mode_):
                                                             train_loss,best_train_loss,best_train_acc*100))
                 logging.info("Epoch loss: {:.4f}, Best train loss: {:.4f}, Best train accuracy: {:.2f}%".format(
                                                             train_loss,best_train_loss,best_train_acc*100))
+                # update learning: learning_rate<-learning_rate/2 if no improvement over last 3 epochs
+                loss_history.append(train_loss)
+                if len(loss_history)>3:
+                    loss_history.pop(0)
+                if best_train_loss==loss_history[0]:
+                    lr = float(lr)/2
+
                 # Perform evaluation
                 if epoch % epochs_per_checkpoint==0:
                     eval_loss_, eval_acc = 0.0, 0.0
@@ -290,8 +312,10 @@ def main(model_archi,train_data, validation_data, test_data, mode_):
         csvfileTest = open('Perf/test_' + str(nn_model) + '.csv', 'w')
         Testwriter = csv.writer(csvfileTest, delimiter=';',)
         Testwriter.writerow(['Test loss'])
+        """
         if not tf.gfile.Exists(DST):
             raise Exception("no weights given")
+        """
         saver.restore(sess, DST)
         # Compute and print results once training is done
         test_loss, test_acc = 0.0, 0.0
@@ -321,7 +345,7 @@ if __name__ == '__main__':
     # Shuffle train data
     np.random.shuffle(train_data)
 
-    #train_data = train_data[:30000]
+    train_data = train_data[:2000]
 
     options, arguments = parser.parse_args(sys.argv)
     if options.mode!="inpainting":
